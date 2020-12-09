5f4aaec41c6c6b3ac70a8f0cd2437f1096f6ed03
deck-app <74038174+deck-app@users.noreply.github.com>
2020-12-09 15:33:03 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 mkdir -p /opt/opencart
 cd /opt/opencart
 curl -sSL https://github.com/opencart/opencart/releases/download/3.0.3.6/opencart-3.0.3.6.zip -o opencart.zip
-echo "41c7e7ec49d389fc4c2204efb40289e6  opencart.zip" | md5sum -c
 unzip opencart.zip 'upload/*' -d /opt/opencart
 mv /opt/opencart/upload/* /var/www/html/
 mv /var/www/html/config-dist.php /var/www/html/config.php
